58757d186ec0adc897bcf906582c5ba9b7597453
gdavid04 <gabordavid04@gmail.com>
2021-05-02 23:54:55 +0200

    Circular connectors, build.properties

--- src/main/java/gdavid/phi/spell/ModPieces.java
+++ src/main/java/gdavid/phi/spell/ModPieces.java
@@ -1,6 +1,8 @@
 package gdavid.phi.spell;
 
 import gdavid.phi.Phi;
+import gdavid.phi.spell.other.ClockwiseConnector;
+import gdavid.phi.spell.other.CounterclockwiseConnector;
 import gdavid.phi.spell.trick.PsionWaveTrick;
 import gdavid.phi.spell.trick.ShadowSequenceTrick;
 import gdavid.phi.spell.trick.ShadowTrick;
@@ -20,17 +22,21 @@ public class ModPieces {
 		
 		public static final String psionManipulation = "psion_manipulation";
 		public static final String opticalMagic = "opticl_magic";
+		public static final String dataFlow = "data_flow";
+		
 	}
 	
 	public static class Params {
 		
 		public static final String speed = PsiAPI.MOD_ID + ".spellparam.speed";
 		public static final String frequency = Phi.modId + ".spellparam.frequency";
+		
 	}
 	
 	public static class Errors {
 		
 		public static final String range = Phi.modId + ".spellerror.range";
+		
 	}
 	
 	@SubscribeEvent
@@ -38,6 +44,8 @@ public class ModPieces {
 		register("trick_psion_wave", PsionWaveTrick.class, Groups.psionManipulation, true);
 		register("trick_shadow", ShadowTrick.class, Groups.opticalMagic, true);
 		register("trick_shadow_sequence", ShadowSequenceTrick.class, Groups.opticalMagic, true);
+		register("connector_clockwise", ClockwiseConnector.class, Groups.dataFlow, true);
+		register("connector_counterclockwise", CounterclockwiseConnector.class, Groups.dataFlow, true);
 	}
 	
 	public static void register(String id, Class<? extends SpellPiece> piece, String group, boolean main) {
---
+++ src/main/java/gdavid/phi/spell/other/ClockwiseConnector.java
@@ -0,0 +1,48 @@
+package gdavid.phi.spell.other;
+
+import vazkii.psi.api.spell.EnumPieceType;
+import vazkii.psi.api.spell.IGenericRedirector;
+import vazkii.psi.api.spell.Spell;
+import vazkii.psi.api.spell.SpellCompilationException;
+import vazkii.psi.api.spell.SpellContext;
+import vazkii.psi.api.spell.SpellParam.Side;
+import vazkii.psi.api.spell.SpellPiece;
+import vazkii.psi.api.spell.SpellRuntimeException;
+
+public class ClockwiseConnector extends SpellPiece implements IGenericRedirector {
+	
+	public ClockwiseConnector(Spell spell) {
+		super(spell);
+	}
+	
+	@Override
+	public Side remapSide(Side side) {
+		return side.rotateCCW();
+	}
+	
+	@Override
+	public String getSortingName() {
+		return "00000000000";
+	}
+	
+	@Override
+	public EnumPieceType getPieceType() {
+		return EnumPieceType.CONNECTOR;
+	}
+	
+	@Override
+	public Class<?> getEvaluationType() {
+		return null;
+	}
+	
+	@Override
+	public Object evaluate() throws SpellCompilationException {
+		return null;
+	}
+	
+	@Override
+	public Object execute(SpellContext context) throws SpellRuntimeException {
+		return null;
+	}
+	
+}
