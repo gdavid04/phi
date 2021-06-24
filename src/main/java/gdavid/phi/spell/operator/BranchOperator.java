64e830b494857b4962c73cbf276f4e62470316e3
gdavid04 <gabordavid04@gmail.com>
2021-06-24 17:24:00 +0200

    Fix Operator: Branch evaluation type crash

--- src/main/java/gdavid/phi/spell/operator/BranchOperator.java
+++ src/main/java/gdavid/phi/spell/operator/BranchOperator.java
@@ -8,6 +8,7 @@ import vazkii.psi.api.spell.SpellCompilationException;
 import vazkii.psi.api.spell.SpellContext;
 import vazkii.psi.api.spell.SpellParam;
 import vazkii.psi.api.spell.SpellParam.Any;
+import vazkii.psi.api.spell.SpellParam.Side;
 import vazkii.psi.api.spell.SpellPiece;
 import vazkii.psi.api.spell.SpellRuntimeException;
 import vazkii.psi.api.spell.param.ParamAny;
@@ -32,16 +33,17 @@ public class BranchOperator extends PieceOperator {
 	
 	@Override
 	public Class<?> getEvaluationType() {
-		if (!isInGrid) return Any.class;
+		// Any is returned in invalid spells only as Psi doesn't handle it properly as an evaluation type
+		if (!isInGrid || paramSides.get(positive) == Side.OFF || paramSides.get(negative) == Side.OFF) return Any.class;
 		try {
 			SpellPiece piece1 = spell.grid.getPieceAtSideWithRedirections(x, y, paramSides.get(positive));
-			if (piece1 == null || ParamHelper.isLoop(piece1)) return Object.class;
+			if (piece1 == null || ParamHelper.isLoop(piece1)) return Any.class;
 			SpellPiece piece2 = spell.grid.getPieceAtSideWithRedirections(x, y, paramSides.get(negative));
-			if (piece2 == null || ParamHelper.isLoop(piece2)) return Object.class;
+			if (piece2 == null || ParamHelper.isLoop(piece2)) return Any.class;
 			Class<?> clazz = TypeHelper.commonSuper(piece1.getEvaluationType(), piece2.getEvaluationType());
 			return clazz == null ? Object.class : clazz;
 		} catch (SpellCompilationException e) {
-			return Object.class;
+			return Any.class;
 		}
 	}
 	
