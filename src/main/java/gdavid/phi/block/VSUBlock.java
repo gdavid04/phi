c7b11a2f7447b6c8c76381c22bdfecddcb88930a
gdavid04 <gabordavid04@gmail.com>
2022-04-14 23:48:15 +0200

    Set VSU contents with Vector Ruler

--- src/main/java/gdavid/phi/block/VSUBlock.java
+++ src/main/java/gdavid/phi/block/VSUBlock.java
@@ -8,13 +8,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import vazkii.psi.common.item.ItemVectorRuler;
 
 public class VSUBlock extends Block {
 	
@@ -30,6 +37,19 @@ public class VSUBlock extends Block {
 	public void addInformation(ItemStack stack, IBlockReader world, List<ITextComponent> tooltip,
 			ITooltipFlag advanced) {
 		tooltip.add(new TranslationTextComponent("block." + Phi.modId + ".vsu.desc"));
+	}
+	
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
+			Hand hand, BlockRayTraceResult rayTraceResult) {
+		ItemStack item = player.getHeldItem(hand);
+		TileEntity tile = world.getTileEntity(pos);
+		if (!(tile instanceof VSUTile)) return ActionResultType.PASS;
+		if (!(item.getItem() instanceof ItemVectorRuler)) return ActionResultType.PASS;
+		if (!world.isRemote) {
+			((VSUTile) tile).setVector(((ItemVectorRuler) item.getItem()).getVector(item));
+		}
+		return ActionResultType.SUCCESS;
 	}
 	
 	@Override
